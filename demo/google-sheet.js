4946dd0e55ab743578f1f493a070059d343f2502
Keerath Jaggi <keeath.jaggi@gmail.com>
2013-12-23 15:51:50 +0530

    google spreadsheet functionality added

--- demo/google-sheet.js
+++ demo/google-sheet.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-		$("#submit").click(function(){
+		$("#submit").click(function(event){
+			event.preventDefault();
 				if($("#link").val() == '')
 				{
 					alert("Please Enter A Link");
@@ -12,8 +13,18 @@ $(document).ready(function(){
 			
 						var newstr = "https://spreadsheets.google.com/feeds/list/"+key+"/od6/public/values?alt=json";
 					
-						$.getJSON("https://spreadsheets.google.com/feeds/list/0AvUcQZ_hmVSPdDhsc0I3b2JTR0NnV1BmcjJVS2VmUWc/od6/public/values?alt=json",function(data){
-							alert("hi");
+						$.getJSON(newstr,function(data){
+							var c =0;
+						for(var i=0; i<data.feed.entry.length; i++)
+						{ 			c=0;
+								$("tr:eq("+(i+1)+") td:eq("+(c++)+")").text(data.feed.entry[i].gsx$store.$t);
+								$("tr:eq("+(i+1)+") td:eq("+(c++)+")").text(data.feed.entry[i].gsx$product.$t);
+								$("tr:eq("+(i+1)+") td:eq("+(c++)+")").text(data.feed.entry[i].gsx$season.$t);
+								$("tr:eq("+(i+1)+") td:eq("+(c++)+")").text(data.feed.entry[i].gsx$sales.$t);
+
+									
+						}
+							
 						});
 				}
 		});
